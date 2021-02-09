4ef7f587a84ac0326f58fe39c8f33ef7c97609b7
lewelyn7 <karol.hamielec@gmail.com>
2021-02-09 14:31:46 +0100

    minor changes

--- simulation.py
+++ simulation.py
@@ -8,7 +8,7 @@ BROKER = ("34.70.234.204", 1883, 60)  # host, port, keepalive
 REAL_BATTERY_TIME = 38 # in days x2, because battery time is updated when measurement is being done
 REAL_MESSAGE_TIME = 3600*24 # 24h in s
 BATTERY_TIME = 38  # seconds
-MESSAGE_TIME = 4  # seconds
+MESSAGE_TIME = 3  # seconds
 MAIN_TOPIC = '/devices/'
 
 
